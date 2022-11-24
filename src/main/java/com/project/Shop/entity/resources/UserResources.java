ebe0da2d840f1e0c4bc485f43744527eebb69387
gregdantas <gregorydantasjc@hotmail.com>
2022-11-23 22:07:36 -0300

    test-endPoint

--- src/main/java/com/project/Shop/entity/resources/UserResources.java
+++ src/main/java/com/project/Shop/entity/resources/UserResources.java
@@ -1,9 +1,15 @@
 package com.project.Shop.entity.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.Shop.entity.User;
 import com.project.Shop.repository.UserRepository;
 
 @RestController
@@ -12,5 +18,11 @@ public class UserResources {
 
 	@Autowired
 	UserRepository repository;
+	
+	@GetMapping(value = "/list")
+	public ResponseEntity<List<User>> list(){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.findAll()) ; 
+	}
+	
 	
 }
