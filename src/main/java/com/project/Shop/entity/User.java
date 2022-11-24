eeab1d23d42e06bd008344d7f4f6e49d36be72b1
gregdantas <gregorydantasjc@hotmail.com>
2022-11-23 21:46:15 -0300

    created

---
+++ src/main/java/com/project/Shop/entity/User.java
@@ -0,0 +1,39 @@
+package com.project.Shop.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name= "TBL_USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id ; 
+	private String name ; 
+	private String phone ; 
+	private String email ; 
+	private String password ; 
+
+
+}
