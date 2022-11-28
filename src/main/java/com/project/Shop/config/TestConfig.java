019766f58782dddb57b8bfa6ae62e3e02686955d
gregdantas <gregorydantasjc@hotmail.com>
2022-11-27 22:43:32 -0300

    OrderStatus

--- src/main/java/com/project/Shop/config/TestConfig.java
+++ src/main/java/com/project/Shop/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.project.Shop.entity.Order;
 import com.project.Shop.entity.User;
+import com.project.Shop.entity.Enums.OrderStatus;
 import com.project.Shop.repository.OrderRepository;
 import com.project.Shop.repository.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User user2 = new User(null, "SECOND USER", "USER@USER2", "9999999", "1111", null);
 
 		repository.saveAll(Arrays.asList(user1, user2));
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),1, user1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), 2, user2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), 1, user1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));	}
 }
--- src/main/java/com/project/Shop/entity/Order.java
+++ src/main/java/com/project/Shop/entity/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.Shop.entity.Enums.OrderStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -28,15 +29,34 @@ import lombok.Setter;
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone  = "GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-    @ManyToOne
-    @JoinColumn(name = "CLIENT_ID")
+
+	private Integer orderStatus;
+
+	@ManyToOne
+	@JoinColumn(name = "CLIENT_ID")
 	private User client;
 
+	public Order(Integer id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 }
